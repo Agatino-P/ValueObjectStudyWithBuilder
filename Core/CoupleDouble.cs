b983be00a2bf87081196e12165e6424f5c52454d
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-11-09 23:29:50 +0100

    Add project files.

---
+++ Core/CoupleDouble.cs
@@ -0,0 +1,93 @@
+﻿using Shared;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class CoupleDouble : ValueObject
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public static bool operator ==(CoupleDouble coupleDoubleOne, CoupleDouble coupleDoubleTwo)
+        {
+            return CoupleDouble.EqualOperator(coupleDoubleOne, coupleDoubleTwo);
+        }
+
+        public static bool operator !=(CoupleDouble coupleDoubleOne, CoupleDouble coupleDoubleTwo)
+        {
+            return CoupleDouble.NotEqualOperator(coupleDoubleOne, coupleDoubleTwo);
+        }
+
+        private CoupleDouble(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public class Builder
+        {
+            private CoupleDouble _coupleDouble;
+            private double? _x;
+            private double? _y;
+            private bool _atLeastOneWrongproperty;
+
+            public Builder()
+            {
+                reset();
+            }
+            public Builder WithX(double X)
+            {
+                _x = X;
+                return this;
+            }
+            public Builder WithY(double Y)
+            {
+                _y = Y;
+                //some fake logic, for test
+                _atLeastOneWrongproperty = _y % 4 == 0;
+                return this;
+            }
+            
+            public Builder WithCopy(CoupleDouble coupleDouble)
+            {
+                _x = coupleDouble.X;
+                _y= coupleDouble.Y;
+                return this;
+            }
+            public CoupleDouble Build()
+            {
+                if (!validData())
+                {
+                    return null;
+                }
+
+                return new CoupleDouble((double)_x, (double)_y);
+            }
+            private bool validData()
+            {
+                if (_atLeastOneWrongproperty)
+                {
+                    return false;
+                }
+
+                if (ReferenceEquals(_x, null) || ReferenceEquals(_y, null)) //Real Logic can go here, if any
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            private void reset()
+            {
+                _coupleDouble = null;
+            }
+
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
---
+++ ValueObjectApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValueObjectApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            //** Invalid because of private constructor
+            //CoupleDouble coupleDouble1 = new CoupleDouble(5, 7);
+            
+            CoupleDouble coupleDouble1  = new CoupleDouble.Builder().WithX(1).WithY(2).Build();
+
+            //** Non ha senso, si usa direttamente coupleDouble1 
+            //coupleDouble2 = new CoupleDouble(coupleDouble1); //if co
+
+            //**Invalid, private setter 
+            //coupleDouble2.X = 4;
+
+            CoupleDouble coupleDouble2 = new CoupleDouble.Builder().WithCopy(coupleDouble1).WithX(10).Build();
+
+            CoupleDouble coupleDouble3 = new CoupleDouble.Builder().WithY(-1).Build();
+
+            CoupleDouble coupleDouble4 = new CoupleDouble.Builder().WithX(1).WithY(2).Build();
+
+            bool areEqual1and2 = (coupleDouble1 == coupleDouble2);
+            bool areEqual1and3 = (coupleDouble1 == coupleDouble2);
+            bool areEqual1and4 = (coupleDouble1 == coupleDouble4);
+
+        }
+    }
+}
